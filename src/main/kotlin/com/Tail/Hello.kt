05d9f6a584aec15c01fc6ab95d53bb0665250ab6
Tai <st30102z@gmail.com>
2020-07-24 23:50:26 +0800

    Initial commit

---
+++ src/main/kotlin/com/Tail/Hello.kt
@@ -0,0 +1,13 @@
+package com.Tail
+
+fun main(args: Array<String>) {
+//    println("Hello kotlin")
+    val h = Human()
+    h.Hello()
+}
+
+class Human{
+    fun Hello(){
+        println("Hello kotln")
+    }
+}
